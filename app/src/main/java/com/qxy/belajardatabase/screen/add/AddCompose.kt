e937086f751d6841832f87b5ad72f91e31462775
Andi Yulistyo <andiyulistyo@gmail.com>
2022-11-18 20:34:30 +0700

    add viewmodel

--- app/src/main/java/com/qxy/belajardatabase/screen/add/AddCompose.kt
+++ app/src/main/java/com/qxy/belajardatabase/screen/add/AddCompose.kt
@@ -3,7 +3,8 @@ package com.qxy.belajardatabase.screen.add
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -20,12 +21,33 @@ fun AddNewsCompose(
             )
         }
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Title") })
-            OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Content") })
-            Button(onClick = {}) {
-                Text(text = "Simpan")
-            }
+        ContentAdd(viewModel = viwModel)
+    }
+}
+
+@Composable
+private fun ContentAdd(modifier: Modifier = Modifier, viewModel: AddViewModel) {
+
+    val title by viewModel.title.observeAsState("")
+    val content by viewModel.content.observeAsState("")
+
+    Column(modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = title,
+            onValueChange = { newValue ->
+                viewModel.setDataTitle(newValue)
+            },
+            label = { Text(text = "Title") })
+
+        OutlinedTextField(
+            value = content,
+            onValueChange = { newValueContent ->
+                viewModel.setDataContent(newValueContent)
+            },
+            label = { Text(text = "Content") })
+
+        Button(onClick = {}) {
+            Text(text = "Simpan")
         }
     }
 }
--- app/src/main/java/com/qxy/belajardatabase/screen/add/AddViewModel.kt
+++ app/src/main/java/com/qxy/belajardatabase/screen/add/AddViewModel.kt
@@ -1,9 +1,31 @@
 package com.qxy.belajardatabase.screen.add
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AddViewModel @Inject constructor() : ViewModel() {
+
+    private val _title = MutableLiveData<String>()
+    val title = _title
+
+    private val _content = MutableLiveData<String>()
+    val content = _content
+
+    fun setDataTitle(newTitle: String) {
+        _title.value = newTitle
+    }
+
+    fun setDataContent(newContent: String) {
+        _content.value = newContent
+    }
+
+    fun submit() {
+
+    }
 }
+
+
+
